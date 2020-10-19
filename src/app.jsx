75ecd059266149c52465fadc81a31487e7715525
feel2174 <feel2174@gmail.com>
2020-10-19 23:16:24 +0900

    make header

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import "./app.css";
-import Navbar from "./components/navbar";
+import styles from "./app.module.css";
 import VideoList from "./components/video_list/video_list";
+import SearchHeader from "./components/search_header/search_header";
 
 function App() {
   const [videos, setVideos] = useState([]);
@@ -11,17 +11,17 @@ function App() {
       method: 'GET',
       redirect: 'follow'
     };
-    
     fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyDyq__QSQktK4TqBUxybuPDLYC9fa1GA90", requestOptions)
       .then(response => response.json())
       .then(result => setVideos(result.items))
       .catch(error => console.log('error', error));
   }, []);
+
   return (
-    <>
-      <Navbar />
+    <div className={styles.app}>
+      <SearchHeader />
       <VideoList videos={videos} />
-    </>
+    </div>
   );
 }
 
